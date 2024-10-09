09db70288f2d91d9cb95ba156377acbb16effbde
Irasubiza2020 <119494139+Irasubiza2020@users.noreply.github.com>
2024-10-09 13:57:42 -0700

    change in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Component/Navbar/Navbar';
-import Hero from './Component/Hero/Hero'; // Ensure this is included for the home page
+import Hero from './Component/Hero/Hero';
 import Project from './Component/Project/Project';
 import AboutUs from './Component/About/AboutUs';
 import Services from './Component/Services/Services';
-import Portfolio from  './Component/Portfolio/portfolio' // Ensure correct casing for Portfolio
-import OurWork from './Pages/ourWork' // Ensure correct path and casing for OurWork
+import Portfolio from './Component/Portfolio/Portfolio'; // Ensure the correct case
+import OurWork from './Pages/OurWork'; // Ensure the correct case
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-     
         <Route path="/" element={
           <>
-            <div id="home"><Hero /></div> 
-            <div id=""><Project /></div>
+            <div id="home"><Hero /></div>
+            <div id="project"><Project /></div> {/* Consider giving it an ID */}
             <div id="about"><AboutUs /></div>
-            <div id="portfolio"><Services /></div>
-            <div id="contact"><Portfolio /></div>
+            <div id="services"><Services /></div> {/* Changed from portfolio to services */}
+            <div id="portfolio"><Portfolio /></div>
           </>
         } />
         {/* Route for the Our Work page */}
