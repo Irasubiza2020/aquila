0cd6e0c8ae078a94d2037076dbb3e7b5b735714a
Irasubiza2020 <119494139+Irasubiza2020@users.noreply.github.com>
2024-10-09 15:52:18 -0700

    arrange  the app

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -43,7 +43,7 @@ class Services extends Component {
   render() {
     return (
       <div id='services' className='mb-6'>
-        <h1 className='text-center text-3xl text-primary font-bold'>
+        <h1 className=' text-3xl text-primary font-bold'>
           <span className='text-third'>Services Offered </span>by Aquila Press
         </h1>
         <div className='flex flex-wrap justify-center'>
